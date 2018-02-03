881eb45a60a63acda8d19801581341ca889c97cf
PDSamson <pdsamson72@gmail.com>
2018-02-03 12:51:19 -0500

    Add updateProject action
    -Add form to project-edit-form
    -Add updateProject action to project-edit-form.hbs, project-edit-form.js,
    project.hbs, and project.js

--- app/routes/project.js
+++ app/routes/project.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model (params) {
    return this.get('store').findRecord('project', params.project_id)
+  },
+  actions: {
+    updateProject(project) {
+      project.save()
+        .then(() => this.get('flashMessages').success('Project Successfully Updated'))
+        .catch(() => this.get('flashMessages').danger('Update Failed'));
+    }
   }
 });
